32b11dd2fa3e5e9a9959b5bfb293b519d2f1c9a5
Sophia <sgigliotti@westmont.edu>
2019-08-10 20:20:15 -0400

    Adding photos in a grid

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import roses from '../photos/roses.jpg';
+import concentration from '../photos/vincent2.jpg';
+import sunset from '../photos/sunset.jpg';
+import fascination from '../photos/fascination.jpg';
+import flamenco from '../photos/flamenco.jpg';
+import getty from '../photos/getty.jpg';
+import kandinsky from '../photos/kandinsky.jpg';
+import home from '../photos/home.jpg';
+import face from '../photos/face2.jpg';
+import dandy from '../photos/dandy.jpg';
+import dante from '../photos/dante_city.jpg';
+import centro from '../photos/elcentro.jpg';
+
 
 const Photos = () => {
   return (
+    <div className="Page-all">
     <div className="Photos">
-      <div>
-        <p>saco las fotos</p>
-      </div>
+      <img src={roses}/>
+      <img src={getty}/>
+      <img src={sunset}/>
+
+      <img src={kandinsky}/>
+      <img src={flamenco}/>
+      <img src={fascination}/>
+
+      <img src={concentration}/>
+      <img src={home}/>
+      <img src={face}/>
+
+      <img src={dante}/>
+      <img src={centro}/>
+      <img src={dandy}/>
+    </div>
     </div>
   );
 }
