0b629cdcef7db2de8c4f9216eb9e6c01f0d96c06
Sophia <sgigliotti@westmont.edu>
2019-08-21 22:22:09 -0400

    using heroku env variables

--- src/.fbConfig.js
+++ src/.fbConfig.js
@@ -4,9 +4,9 @@ const config = {
     authDomain: process.env.AUTH_DOMAIN,
     databaseURL: process.env.DB_URL,
     projectId: process.env.PROJECT_ID,
-    storageBucket: "wander-68b9c.appspot.com",
-    messagingSenderId: "601796006663",
-    appId: "1:601796006663:web:771d33ff21c58480" 
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID 
 };
 
 export default config;
