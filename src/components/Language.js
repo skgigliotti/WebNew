d74a4cdc1dc9f5732e8d43208dd23bf054c895df
Sophia <sgigliotti@westmont.edu>
2019-08-27 08:53:13 -0500

    adding writing sample

--- src/components/Language.js
+++ src/components/Language.js
@@ -2,7 +2,7 @@ import React from 'react';
 import firebase from '../base.js';
 
 const Language = () => {
-  const writing = '';
+  var writing = '';
   const sample = firebase.database().ref('sample');
   sample.on('value', (snapshot) => {
     console.log(sample);
