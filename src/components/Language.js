40fc2c43c0eb480cfdd96e6a57cbb2518a65b809
Sophia <sgigliotti@westmont.edu>
2019-09-11 21:48:12 -0500

    pulling writing smaple using class

--- src/components/Language.js
+++ src/components/Language.js
@@ -1,48 +1,62 @@
 import React from 'react';
 import firebase from '../base.js';
 
-const Language = () => {
-  var writing = '';
-  const sample = firebase.database().ref('sample');
+class Language extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      writing: ''
+    }
+  }
+
+  componentDidMount(){
+    const sample = firebase.database().ref('sample');
   sample.on('value', (snapshot) => {
     
     let sampleWriting = snapshot.val();
-    writing = sampleWriting.sor_juana;
+    this.setState({writing: sampleWriting.sor_juana});
   }
       );
-  return (
-    <div className="Page-all">
-      <h1>Language</h1>
-      <div className="Language">
-        <div className="Text-background">
-        <h2>Courses</h2>
-      <ul>
-    <li>Advanced Spanish Language and Grammar</li>
-    <li>Latin American Cultures </li>
-    <li>Introduction to Hispanic Literature</li>
-    <li>The Cradle of Magic Realism</li>
-  </ul>
-  </div>
-  <div className="Text-background">
-    <h2>Cross Cultural Engagement</h2>
-  <ul>
-    <li>Weekly volunteer working with international students in Santa Barbara.
-    Facilitate intercultural dialogue
-    and help international students with conversational English.</li>
+  }
   
-    <li>Au pair in Seville, Spain. Taught English to two young girls.
-      
-      Improved my ability to communicate in Spanish.</li>
+  render(){
 
-  </ul>
-    
-  </div>
-  <div className="Text-background Example">
-    <h2>Por ejemplo...</h2>
-  <p>{writing}</p>
-  </div>
-      </div>
+    return (
+      <div className="Page-all">
+        <h1>Language</h1>
+        <div className="Language">
+          <div className="Text-background">
+          <h2>Courses</h2>
+        <ul>
+      <li>Advanced Spanish Language and Grammar</li>
+      <li>Latin American Cultures </li>
+      <li>Introduction to Hispanic Literature</li>
+      <li>The Cradle of Magic Realism</li>
+    </ul>
     </div>
-  );
+    <div className="Text-background">
+      <h2>Cross Cultural Engagement</h2>
+    <ul>
+      <li>Weekly volunteer working with international students in Santa Barbara.
+      Facilitate intercultural dialogue
+      and help international students with conversational English.</li>
+    
+      <li>Au pair in Seville, Spain. Taught English to two young girls.
+        
+        Improved my ability to communicate in Spanish.</li>
+  
+    </ul>
+      
+    </div>
+    <div className="Text-background Example">
+      <h2>Por ejemplo...</h2>
+    <p>{this.state.writing}</p>
+    </div>
+        </div>
+      </div>
+    );
+
+  }
+  
 }
 export default Language
