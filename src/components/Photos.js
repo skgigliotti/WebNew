3f691a42dcfc466842c325caa5d80cb3b9255f1c
Sophia <sgigliotti@westmont.edu>
2019-10-26 21:17:54 -0500

    pulling gallery photos from database instead of uploads

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -22,7 +22,7 @@ class Photos extends React.Component {
   }
 
   componentDidMount() { 
-    storage.ref("gallery/").then((snapshot) => this.setState({images: snapshot.val()}).catch(error => console.error(error)))
+    storage.ref("gallery/").on("value", ((snapshot) => this.setState({images: snapshot.val()}).catch(error => console.error(error))))
    }
   
   render(){
