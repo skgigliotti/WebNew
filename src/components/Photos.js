a7c0b408efdbe8c1e36f7399470b048ed0c73616
Sophia <sgigliotti@westmont.edu>
2019-10-27 09:48:38 -0600

    pulling gallery photos from database instead of uploads

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import Modal from './Modal.js';
 import firebase from '../base.js';
+import roses from '../photos/roses.jpg';
+import concentration from '../photos/vincent2.jpg';
+import sunset from '../photos/sunset.jpg';
+import fascination from '../photos/fascination.jpg';
+import flamenco from '../photos/flamenco.jpg';
+import getty from '../photos/getty.jpg';
+import kandinsky from '../photos/kandinsky.jpg';
+import home from '../photos/home.jpg';
+import face from '../photos/face2.jpg';
+import dandy from '../photos/dandy.jpg';
+import dante from '../photos/dante_city.jpg';
+import centro from '../photos/elcentro.jpg';
+import saginaw from '../photos/saginaw.jpg';
+import boston from '../photos/boston.jpg';
+import sierra from '../photos/sierra.jpg';
+import met from '../photos/met.jpg';
+import self from '../photos/selfport.jpg';
+import pom from '../photos/pom.jpg';
+import corgui from '../photos/corgui.jpg';
+import flor from '../photos/flor.jpg';
+
+const photos = [roses, getty, sunset, kandinsky, flamenco, fascination,
+  concentration, saginaw, face, sierra, centro, met, pom, corgui, flor, boston, dante, home, self, dandy];
 
 require('dotenv').config();
+
+
 
 
 function handleClick(e) {
@@ -48,7 +73,7 @@ class Photos extends React.Component {
       <div className="Page-all">
         
       <div className="Photos">
-      {this.state.images.map((p) => (<img src={p} alt={p} onClick={handleClick}/>))}
+      {photos.map((p) => (<img src={p} alt={p} onClick={handleClick}/>))}
       </div>
       
       <Modal />
