306e5f3fc0e6831766dd8f2f8e106d73fa1568d0
Sophia <sgigliotti@westmont.edu>
2019-10-26 21:30:20 -0500

    pulling gallery photos from database instead of uploads

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Modal from './Modal.js';
 import firebase from '../base.js';
-import {storage} from '../base.js';
 
 require('dotenv').config();
 
@@ -22,7 +21,20 @@ class Photos extends React.Component {
   }
 
   componentDidMount() { 
-    firebase.storage.ref("gallery/").on("value", ((snapshot) => this.setState({images: snapshot.val()}).catch(error => console.error(error))))
+    // Import Admin SDK
+    var admin = require("firebase-admin");
+
+    // Get a database reference to our posts
+    var db = admin.database();
+    var ref = db.ref("storage/gallery");
+
+    // Attach an asynchronous callback to read the data at our posts reference
+    ref.on("value", function(snapshot) {
+    console.log(snapshot.val());
+    }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+    });
+    
    }
   
   render(){
