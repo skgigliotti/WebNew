0cd9a916e67de023311044d5f28b45d05b3d248c
Sophia <sgigliotti@westmont.edu>
2020-02-01 17:07:34 -0800

    content update to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,14 +13,14 @@ const Home = () => {
         <div>
         <p>
           I am a senior at Westmont College in Santa Barbara, CA. I study Computer Science and Spanish
-          and enjoy the crossover of these two fields. I am seeking opportunities to continue working in the field of education technology.
+          and enjoy the crossover of these two fields. My long-term goal is to teach computer science to primary and secondary students. 
+          I am determined to involve and empower young students from underrepresented groups to engage with technology creation. 
           I am commited to ethical computing and am passionate
           about seeking justice and equity through technology and otherwise.</p>
           
-          <p>Most recently, I worked as an intern at EF Education First in 
+          <p>Currently, I work as a Salesforce Developer and hold a Platform Developer I certification. I get the opportunity to build out innovative solutions for the administration at my college. 
+            I have also worked as a developer intern at EF Education First in 
           Boston, MA. At EF, I got to focus on improving and developing technology for student travelers.
-          I also have a strong math background and professional experience
-          working with databases and Salesforce. 
         </p>
         
         <p>
