f234fb2ee2247739732d0debdaed086d8ed73e78
Sophia <sgigliotti@westmont.edu>
2020-07-03 20:52:30 -0700

    Update about me

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,37 +4,35 @@ import profile from '../photos/profile.JPG';
 const Home = () => {
   return (
     <div className="Page-all">
-        
+
         <h1>
-          About Me
+          Hello World
         </h1>
-        
+
         <div className="Home Text-background">
         <div>
         <p>
-          I am a senior at Westmont College in Santa Barbara, CA. I study Computer Science and Spanish
-          and enjoy the crossover of these two fields. My long-term goal is to teach computer science to primary and secondary students. 
-          I am determined to involve and empower young students from underrepresented groups to engage with technology creation. 
-          I am commited to ethical computing and am passionate
-          about seeking justice and equity through technology and otherwise.</p>
-          
-          <p>Currently, I work as a Salesforce Developer and hold a Platform Developer I certification. I get the opportunity to build out innovative solutions for the administration at my college. 
-            I have also worked as a developer intern at EF Education First in 
-          Boston, MA. At EF, I got to focus on improving and developing technology for student travelers.
+          I am a Computer Scientist and a language learner.
+          My long-term goal is to teach computer science to primary and secondary students.
+          I am determined to involve and empower young students from underrepresented groups to engage with technology creation.
+          If we truly want to be building solutions that benefit more people, it's time to prioritize and value diversity.
+          I am passionate about seeking justice and equity through technology and otherwise.</p>
+
+          <p>Professionally, I work as a Junior Software Developer at Outside Open.
         </p>
-        
+
         <p>
-          When I am not programming or reading great Spanish literature, you'll find me running, taking photos,
-          or knitting.
+          When I am not programming, you'll find me running, taking photos,
+          or reading great Spanish literature.
         </p>
         </div>
         <div className="Profile">
       <img src={profile} style={{width: '75%'}} title="Here I am traveling in Portland, ME. Camera in hand as always!"/>
       </div>
-  
+
       </div>
-     
-      
+
+
     </div>
   );
 }
