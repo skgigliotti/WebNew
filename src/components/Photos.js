fd3f7d53be128c1d67178d30be3571bedb6b6e9c
Sophia <sgigliotti@westmont.edu>
2020-07-03 20:58:08 -0700

    Adding photos

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -8,7 +8,7 @@ import PhotoDisplay from './PhotoDisplay.js'
 require('dotenv').config();
 
 const photos = ['roses', 'getty', 'sunset', 'kandinsky', 'flamenco', 'fascination',
-  'vincent2', 'saginaw', 'face2', 'sierra', 'elcentro', 'met', 'pom', 'corgui', 'flor', 'boston', 'dante_city', 'home', 'hda', 'ba','patio','puebla','sanmig','classmx','cruz','dandy'];
+  'vincent2', 'saginaw', 'face2', 'vine', 'hacking', 'sierra', 'elcentro', 'met', 'pom', 'corgui', 'flor', 'boston', 'dante_city', 'home', 'hda', 'ba','patio','puebla','sanmig','classmx','cruz','dandy'];
 
 
 
