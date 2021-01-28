ee71306c30760d0831fee3763f2d06a3f09f3107
Sophia Gigliotti <skgigliotti@gmail.com>
2021-01-28 00:03:24 -0800

    WIP add social icons

--- src/App.js
+++ src/App.js
@@ -1,40 +1,50 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Menu from './components/Menu.js';
-
 import Home from './components/Home.js'
 import ComputerScience from './components/ComputerScience.js';
 import Language from './components/Language.js';
 import Photos from './components/Photos.js';
 import Wander from './components/Wander.js';
+import { SocialIcon } from 'react-social-icons';
+const Spacer = require('react-spacer')
+
 
 
 
 
 class App extends React.Component {
-  render(){
+  render() {
     return (
-   <div className="rectangle">
-      <BrowserRouter>
-          <div className="Title">
+      <div className="container">
+        <div className="rectangle">
+          <BrowserRouter>
+            <div className="Title">
               Sophia Gigliotti
             </div>
-          <div>
-            <Menu/>
-          </div>
-          <div className="App">
-            <Route exact path="/" component={Home} />
-            <Route path="/computerScience" component={ComputerScience} />
-            <Route path="/language" component={Language} />
-            <Route path="/photos&design" component={Photos} />
-            <Route path="/wander" component={Wander} />
-          </div>
-      </BrowserRouter>
-      </div>
+            <div>
+              <Menu />
+
+            </div>
+            <div className="App">
+              <Route exact path="/" component={Home} />
+              <Route path="/computerScience" component={ComputerScience} />
+              <Route path="/language" component={Language} />
+              <Route path="/photos&design" component={Photos} />
+              <Route path="/wander" component={Wander} />
+            </div>
+          </BrowserRouter>
+        </div>
+        <div className="icons">
+          <SocialIcon url="https://www.linkedin.com/in/sophia-gigliotti/" bgColor="#bbb" />
+          <SocialIcon url="https://github.com/skgigliotti" bgColor="#bbb" />
+          <SocialIcon url="http://twitter.com/sophtwaredev" bgColor="#bbb" />
+        </div>
+      </div >
     );
   }
-  
+
 }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,32 @@
 import React from 'react';
 import profile from '../photos/profile.JPG';
+import '../App.css';
 
 const Home = () => {
   return (
     <div className="Page-all">
 
-        <h1>
-          Hello World
-        </h1>
+      <h1>Hello World</h1>
 
-        <div className="Home Text-background">
+      <div className="Home Text-background">
         <div>
-        <p>
-          I am a Computer Scientist and a language learner.
-          My long-term goal is to teach computer science to primary and secondary students.
-          I am determined to involve and empower young students from underrepresented groups to engage with technology creation.
+          <p>
+            I am a computer scientist and a language learner.
+            My long-term goal is to teach computer science to primary and secondary students.
+            I am determined to involve and empower young students from underrepresented groups to engage with technology creation.
           I am passionate about seeking justice and equity through technology and otherwise.</p>
 
-          <p>Professionally, I work as a Junior Software Developer at Outside Open in Santa Barbara, CA.
+          <p>Professionally, I work as a Full Stack Software Developer at Outside Open in Santa Barbara, CA.
         </p>
 
-        <p>
-          When I am not programming, you'll find me running, taking photos,
-          or reading great Spanish literature.
+          <p>
+            When I am not programming, you'll find me running, taking photos,
+            or reading great Spanish literature.
         </p>
         </div>
         <div className="Profile">
-      <img src={profile} style={{width: '75%'}} title="Here I am traveling in Portland, ME. Camera in hand as always!"/>
-      </div>
+          <img src={profile} style={{ width: '75%' }} title="Here I am traveling in Portland, ME. Camera in hand as always!" />
+        </div>
 
       </div>
 
