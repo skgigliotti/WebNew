6dd490766b147ccc9074f8c371a2d3171f2fedf5
Sophia Gigliotti <skgigliotti@gmail.com>
2021-02-02 21:53:52 -0800

    Social icons for heroku

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './Components.css';
-// import { SocialIcon } from 'react-social-icons';
+import { SocialIcon } from 'react-social-icons';
+
 
 class Footer extends React.Component {
     render() {
         return (
+
             <div className="footer">
                 <div className="icons">
-                    {/* <SocialIcon url="https://www.linkedin.com/in/sophia-gigliotti/" bgColor="#bbb" />
+
+                    <SocialIcon url="https://www.linkedin.com/in/sophia-gigliotti/" bgColor="#bbb" />
                     <SocialIcon url="https://github.com/skgigliotti" bgColor="#bbb" />
-                    <SocialIcon url="http://twitter.com/sophtwaredev" bgColor="#bbb" /> */}
+                    <SocialIcon url="http://twitter.com/sophtwaredev" bgColor="#bbb" />
                 </div>
             </div>
 
