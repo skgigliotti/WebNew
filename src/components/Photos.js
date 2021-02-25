65302fffcb958048aeb9d069743a30c44449ed05
Sophia Gigliotti <skgigliotti@gmail.com>
2021-02-24 22:41:12 -0800

    Add portraits

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -6,12 +6,22 @@ import PhotoDisplay from './PhotoDisplay.js'
 
 require('dotenv').config();
 
-const photos = ['getty', 'sunset', 'flamenco', 'fascination',
+const photos = ['getty', 'sunset', 'flamenco',
   'saginaw', 'vine', 'hacking', 'sierra', 'elcentro', 'met', 'pom', 'corgui', 'flor', 'boston', 'home', 'hda',
   'ba', 'patio', 'puebla', 'sanmig', 'classmx', 'cruz', 'wall', 'leon', 'doggo', 'red', 'icecream', 'bridge', 'golden_soap', 'fire', 'beach_xmas'];
 
+const portraits = ['mm',
+  'fence_trav', 'flower_chena', 'side_trav', 'fascination',
+  'tree_chena', 'leaves_ben', 'court_ben', 'rebeca'];
+
+const v_portraits = ['shoe_trav',
+  'shadow_bro', 'reflection_trav'];
+
 const graphics = ['roses', 'kandinsky',
-  'vincent2', 'rmw', 'face2', 'dante_city',
+  'vincent2', 'face2', 'dante_city',
+];
+
+const v_graphics = [
   'dandy'];
 
 
@@ -24,8 +34,10 @@ class Photos extends React.Component {
   constructor() {
     super();
     this.state = {
-
       photos: [],
+      portraits: [],
+      v_portraits: [],
+      v_graphics: [],
       graphics: [],
       ready: false
 
@@ -43,6 +55,56 @@ class Photos extends React.Component {
     var storage = firebase.storage();
     photos.map((p) => ((storage.ref('gallery/' + p + '.jpg')).getDownloadURL().then((url) =>
       this.state.photos.push(url)
+    ).catch(function (error) {
+      console.log(error)
+      // A full list of error codes is available at
+      // https://firebase.google.com/docs/storage/web/handle-errors
+      switch (error.code) {
+        case 'storage/object-not-found':
+          // File doesn't exist
+          break;
+
+        case 'storage/unauthorized':
+          // User doesn't have permission to access the object
+          break;
+
+        case 'storage/canceled':
+          // User canceled the upload
+          break;
+
+        case 'storage/unknown':
+          // Unknown error occurred, inspect the server response
+          break;
+      }
+    }).then(() => this.setState({ ready: true }))))
+
+    portraits.map((p) => ((storage.ref('gallery/' + p + '.jpg')).getDownloadURL().then((url) =>
+      this.state.portraits.push(url)
+    ).catch(function (error) {
+      console.log(error)
+      // A full list of error codes is available at
+      // https://firebase.google.com/docs/storage/web/handle-errors
+      switch (error.code) {
+        case 'storage/object-not-found':
+          // File doesn't exist
+          break;
+
+        case 'storage/unauthorized':
+          // User doesn't have permission to access the object
+          break;
+
+        case 'storage/canceled':
+          // User canceled the upload
+          break;
+
+        case 'storage/unknown':
+          // Unknown error occurred, inspect the server response
+          break;
+      }
+    }).then(() => this.setState({ ready: true }))))
+
+    v_portraits.map((p) => ((storage.ref('gallery/' + p + '.jpg')).getDownloadURL().then((url) =>
+      this.state.v_portraits.push(url)
     ).catch(function (error) {
       console.log(error)
       // A full list of error codes is available at
@@ -91,6 +153,31 @@ class Photos extends React.Component {
       }
     }).then(() => this.setState({ ready: true }))))
 
+    v_graphics.map((p) => ((storage.ref('gallery/' + p + '.jpg')).getDownloadURL().then((url) =>
+      this.state.v_graphics.push(url)
+    ).catch(function (error) {
+      console.log(error)
+      // A full list of error codes is available at
+      // https://firebase.google.com/docs/storage/web/handle-errors
+      switch (error.code) {
+        case 'storage/object-not-found':
+          // File doesn't exist
+          break;
+
+        case 'storage/unauthorized':
+          // User doesn't have permission to access the object
+          break;
+
+        case 'storage/canceled':
+          // User canceled the upload
+          break;
+
+        case 'storage/unknown':
+          // Unknown error occurred, inspect the server response
+          break;
+      }
+    }).then(() => this.setState({ ready: true }))))
+
 
   }
 
@@ -99,7 +186,10 @@ class Photos extends React.Component {
       return (
         <div>
           <PhotoDisplay images={this.state.photos} />
+          <PhotoDisplay images={this.state.portraits} />
+          <PhotoDisplay images={this.state.v_portraits} />
           <PhotoDisplay images={this.state.graphics} />
+          <PhotoDisplay images={this.state.v_graphics} />
         </div>
       )
     } else {
